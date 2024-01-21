afd7b9a9ea17159b88d6d09b98b79eff598a878f
suntisfied <147501927+suntisfied@users.noreply.github.com>
2024-01-21 14:16:15 +0900

    fix(test_user_api): correct typo

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -35,7 +35,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
-        self.assertNotIN('password', res.data)
+        self.assertNotIn('password', res.data)
 
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
